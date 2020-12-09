8de736d779c106ecffe3a2f784c727a594dd2ab8
ianwow <ianwow@amazon.com>
2020-12-08 22:26:10 -0800

    validate screenshots

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const sleep = promisify(setTimeout)
 
 let url = process.env.WEBAPP_URL;
 (async () => {
-    const tester = await ScreenshotTester(0.8, false, false, [], {
+    const test_screenshot = await ScreenshotTester(0.8, false, false, [], {
         transparency: 0.5
     })
+
+    const test_screenshot_containing_video = await ScreenshotTester(
+      0.1, // threshold
+      false, // anti-aliasing
+      false, // ignore colors
+      {
+          ignoreRectangles: [[670, 95, 583, 331]],
+      }, // rectangles
+      {
+          transparency: 0.5
+      }
+    )
 
     console.log("Loading " + url)
     // Chromium doesn't support video playback, so use Chrome instead
@@ -20,7 +32,11 @@ let url = process.env.WEBAPP_URL;
     await page.type('input','ianwow@amazon.com')
     // Type in the password
     await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div:nth-child(2) > input','Password123@')
-    await page.screenshot({path: 'screenshot00_authentication.png'})
+    // await page.screenshot({path: 'screenshot00_authentication.png'})
+    console.log("validating auth form: "+await test_screenshot(page, 'screenshot00_authentication', {
+        fullPage: true,
+    }))
+
     console.log("Page title: " + await page.title())
     console.log("authenticating...")
     // click Login button
@@ -29,14 +45,17 @@ let url = process.env.WEBAPP_URL;
       page.waitForNavigation({ waitUntil: 'networkidle0' }),
     ]);
 
-    console.log('validating collection view...')
     try {
         await page.waitForSelector('tbody > tr:nth-child(1) > td:nth-child(3) > a')
     } catch (e) {
         console.log('workflow status does not exist')
     }
     await page.waitForTimeout(1000)
-    await page.screenshot({path: 'screenshot01_collection_view.png'})
+    // await page.screenshot({path: 'screenshot01_collection_view.png'})
+    console.log("validating collection view: "+await test_screenshot(page, 'screenshot01_collection_view', {
+        fullPage: true,
+    }))
+
     // get workflow status and wait if its not complete
     const workflowStatusSelector = 'tbody > tr:nth-child(1) > td:nth-child(3) > a'
     await page.waitForSelector(workflowStatusSelector,  {polling:1000, timeout: 3000})
@@ -44,8 +63,8 @@ let url = process.env.WEBAPP_URL;
         // Wait up to 5 seconds for the asset list to load
         await page.waitForFunction(selector => !!document.querySelector(selector), {polling:1000, timeout: 3000}, workflowStatusSelector);
         let workflowStatus = await page.evaluate(selector => document.querySelector(selector).innerText, workflowStatusSelector);
-        // wait up to 30 minutes for workflow to complete
-        let i = 30
+        // wait up to 45 minutes for workflow to complete
+        let i = 45
         console.log("Waiting for workflow to complete...")
         while (workflowStatus != "Complete" && i > 0) {
             // print the workflow status
@@ -60,7 +79,6 @@ let url = process.env.WEBAPP_URL;
     }
 
     // VALIDATE OBJECTS TAB
-    console.log("validating objects tab...")
     await page.click('tbody > tr:nth-child(1) > td:nth-child(6) > a');
     // wait until video loads
     const video_selector='#videoPlayer > div.vjs-control-bar > div.vjs-remaining-time.vjs-time-control.vjs-control > span.vjs-remaining-time-display'
@@ -70,20 +88,26 @@ let url = process.env.WEBAPP_URL;
         await page.waitForSelector(rounded_button_selector, {timeout: 5000})
     } catch(e) {
         // no data in this tab
-    }    await page.screenshot({path: 'screenshot02_tab_objects.png'})
+    // }    await page.screenshot({path: 'screenshot02_tab_objects.png'})
+        console.log("validating objects: "+await test_screenshot_containing_video(page, 'screenshot02_tab_objects', {
+            fullPage: true,
+        }))
+    }
 
     // VALIDATE CELEBRITY TAB
-    console.log("validating celebrities tab...")
     let tab_selector='#__BVID__28___BV_tab_button__'
     await page.click(tab_selector)
     try {
         await page.waitForSelector(rounded_button_selector, {timeout: 5000})
     } catch(e) {
         // no data in this tab
-    }    await page.screenshot({path: 'screenshot03_tab_celebrities.png'})
+    }
+    // await page.screenshot({path: 'screenshot03_tab_celebrities.png'})
+    console.log("validating celebrities: "+await test_screenshot_containing_video(page, 'screenshot03_tab_celebrities', {
+        fullPage: true,
+    }))
 
     // VALIDATE MODERATION TAB
-    console.log("validating moderation tab...")
     tab_selector='#__BVID__30___BV_tab_button__'
     await page.click(tab_selector)
     try {
@@ -91,44 +115,56 @@ let url = process.env.WEBAPP_URL;
     } catch(e) {
         // no data in this tab
     }
-    await page.screenshot({path: 'screenshot04_tab_moderation.png'})
+    // await page.screenshot({path: 'screenshot04_tab_moderation.png'})
+    console.log("validating moderation: "+await test_screenshot_containing_video(page, 'screenshot04_tab_moderation', {
+        fullPage: true,
+    }))
 
     // VALIDATE FACES TAB
-    console.log("validating faces tab...")
     tab_selector='#__BVID__32___BV_tab_button__'
     await page.click(tab_selector)
     try {
         await page.waitForSelector(rounded_button_selector, {timeout: 5000})
     } catch(e) {
         // no data in this tab
-    }    await page.screenshot({path: 'screenshot05_tab_faces.png'})
+    }
+    // await page.screenshot({path: 'screenshot05_tab_faces.png'})
+    console.log("validating faces: "+await test_screenshot_containing_video(page, 'screenshot05_tab_faces', {
+        fullPage: true,
+    }))
 
     // VALIDATE WORDS TAB
-    console.log("validating words tab...")
     tab_selector='#__BVID__34___BV_tab_button__'
     await page.click(tab_selector)
     try {
         await page.waitForSelector(rounded_button_selector, {timeout: 3000})
     } catch(e) {
         // no data in this tab
-    }    await page.screenshot({path: 'screenshot06_tab_words.png'})
+    }
+    // await page.screenshot({path: 'screenshot06_tab_words.png'})
+    console.log("validating words: "+await test_screenshot_containing_video(page, 'screenshot06_tab_words', {
+        fullPage: true,
+    }))
 
     // VALIDATE CUES TAB
-    console.log("validating cues tab...")
     tab_selector='#__BVID__36___BV_tab_button__'
     await page.click(tab_selector)
     await page.waitForTimeout(3000)
-    await page.screenshot({path: 'screenshot07_tab_cues.png'})
+    // await page.screenshot({path: 'screenshot07_tab_cues.png'})
+    console.log("validating cues: "+await test_screenshot_containing_video(page, 'screenshot07_tab_cues', {
+        fullPage: true,
+    }))
 
     // VALIDATE SHOTS TAB
-    console.log("validating shots tab...")
     tab_selector='#__BVID__38___BV_tab_button__'
     await page.click(tab_selector)
     await page.waitForTimeout(3000)
-    await page.screenshot({path: 'screenshot08_tab_shots.png'})
+    // await page.screenshot({path: 'screenshot08_tab_shots.png'})
+    console.log("validating shots: "+await test_screenshot_containing_video(page, 'screenshot08_tab_shots', {
+        fullPage: true,
+    }))
 
     // VALIDATE UPLOAD PAGE
-    console.log("validating upload page...")
     await page.goto(url + "/upload", {waitUntil: 'load'});
     await page.waitForSelector('#app > div > div.container > div.container > div > div > button.btn.m-1.btn-secondary.collapsed', {
         visible: true,
@@ -138,19 +174,22 @@ let url = process.env.WEBAPP_URL;
     // Does the configure workflow form look right?
     await page.click('#app > div > div.container > div.container > div > div > button.btn.m-1.btn-secondary.collapsed');
     await page.waitForTimeout(500)
-    await page.screenshot({path: 'screenshot09_configure_workflow_form_default.png'})
-    console.log('validating workflow config form...')
+    // await page.screenshot({path: 'screenshot09_configure_workflow_form_default.png'})
+    console.log("validating upload page: "+await test_screenshot_containing_video(page, 'screenshot09_configure_workflow_form_default', {
+        fullPage: true,
+    }))
 
     await page.click('#collapse-2 > div > div:nth-child(2) > button:nth-child(2)');
     await page.waitForTimeout(500)
-    await page.screenshot({path: 'screenshot10_configure_workflow_form_clear_all.png'})
+    // await page.screenshot({path: 'screenshot10_configure_workflow_form_clear_all.png'})
+    console.log("validating workflow config form: "+await test_screenshot_containing_video(page, 'screenshot10_configure_workflow_form_clear_all', {
+        fullPage: true,
+    }))
 
     await page.click('#collapse-2 > div > div:nth-child(2) > button:nth-child(1)');
     await page.waitForTimeout(500)
-    await page.screenshot({path: 'screenshot11_configure_workflow_form_select_all.png'})
-
-    console.log("Screenshot validated?")
-    console.log(await tester(page, 'test2', {
+    // await page.screenshot({path: 'screenshot11_configure_workflow_form_select_all.png'})
+    console.log("validating clear all: "+await test_screenshot(page, 'screenshot11_configure_workflow_form_select_all', {
         fullPage: true,
     }))
 
