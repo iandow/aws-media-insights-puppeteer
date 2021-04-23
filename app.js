278774ee17e8e048be7e4632a2de08a74bc5d0ee
ianwow <ianwow@amazon.com>
2021-04-23 12:18:22 -0700

    validate workflow config via screenshot

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ let temp_password = process.env.TEMP_PASSWORD;
 
     console.log("Loading " + url)
     // Chromium doesn't support video playback, so use Chrome instead
+    // uncomment this when testing on a laptop:
     // const browser = await  puppeteer.launch({executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', headless: true})
     const browser = await  puppeteer.launch({executablePath: '/usr/bin/google-chrome', headless: true, args: ['--no-sandbox']})
     const page = await browser.newPage();
@@ -58,14 +59,12 @@ let temp_password = process.env.TEMP_PASSWORD;
     await page.type('input','s3sink@bigendiandata.com')
     // Type in the password
     await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div:nth-child(2) > input', temp_password)
-    console.log("Validating auth form: "+await test_screenshot(page, 'screenshot00_authentication', {
-        fullPage: true,
-    }))
-
-    // await page.screenshot({path: 'screenshot00_authentication.png'})
+    // console.log("Validating auth form: "+await test_screenshot(page, 'screenshot00_authentication', {
+    //     fullPage: true,
+    // }))
 
     console.log("Page title: " + await page.title())
-    console.log("Authenticating...")
+    console.log("Authenticating")
     // click Login button
     await Promise.all([
       page.click("button"),
@@ -73,42 +72,35 @@ let temp_password = process.env.TEMP_PASSWORD;
       // page.waitForNavigation({ waitUntil: 'networkidle0' }),
     ]);
 
-    // // wait for password reset form
-    // try {
-    //   // Wait up to 3 seconds for the password reset form to load
-    //   passwordResetSelector = '#app > div > div > div > div.Section__sectionBody___3DCrX > div > input'
-    //   await page.waitForFunction(selector => !!document.querySelector(selector), {polling:1000, timeout: 3000}, passwordResetSelector);
-    //   // enter new password
-    //   console.log("validating password reset form: " + await test_screenshot(page, 'screenshot00_password_reset', {
-    //     fullPage: true,
-    //   }))
-    //   await page.waitForTimeout(1000)
-
       // Are we on the password reset page?
       passwordHeaderSelector = "#app > div > div > div > div.Section__sectionHeader___13iO4"
-      if (await page.$(passwordHeaderSelector) !== null) {
-        console.log("found password reset form")
-        // enter new password
-        await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div > input', temp_password)
-        console.log("submitting new password...")
-        // click Submit button
-        await Promise.all([
-          page.click("button"),
-          page.waitForTimeout(2000)
-        ]);
-      } else {
+      try {
+        if (await page.$(passwordHeaderSelector) !== null) {
+          console.log("found password reset form")
+          // enter new password
+          await page.type('#app > div > div > div > div.Section__sectionBody___3DCrX > div > input', temp_password)
+          console.log("submitting new password")
+          // click Submit button
+          await Promise.all([
+            page.click("button"),
+            page.waitForTimeout(2000)
+          ]);
+        } else {
+          console.log('Temporary password has already been reset.')
+        }
+      } catch(e) {
         console.log('Temporary password has already been reset.')
       }
 
     // Wait for catalog view to load
-    console.log("Waiting 5s for catalog view to load...")
+    console.log("Waiting 5s for catalog view to load")
     await page.waitForTimeout(5000)
 
     // Validate that the catalog table is not empty
     tableEmptyMessageSelector = "tbody > tr > td > div > div"
     try {
       if (await page.$(tableEmptyMessageSelector) !== null) {
-        const text = await page.$eval("tbody > tr > td > div > div", el => el.textContent);
+        const text = await page.$eval(tableEmptyMessageSelector, el => el.textContent);
         if (text === 'There are no records to show') {
           throw new Error("There are no records to show.")
         }
@@ -120,7 +112,7 @@ let temp_password = process.env.TEMP_PASSWORD;
 
     // Validate that the workflow status for the first asset in the
     // table says, "Complete"
-    console.log("Validating that the first asset has status, 'Complete'")
+    console.log("Validating that the first asset's workflow is Complete")
     const assetWorkflowStatusSelector = "td.tableWordWrap:nth-child(3) > a:nth-child(1)"
     try {
       await page.waitForSelector(assetWorkflowStatusSelector, {polling:1000, timeout: 3000})
@@ -129,45 +121,14 @@ let temp_password = process.env.TEMP_PASSWORD;
     }
     try {
       const text = await page.$eval(assetWorkflowStatusSelector, el => el.textContent);
+      if (text !== 'Complete') {
+        console.log("Asset workflow status: " + text)
+        throw new Error("Asset workflow status is not Complete")
+      }
     } catch (e) {
-      console.assert(text === 'Complete', "Asset workflow status does not have the expected value, 'Complete'")
+      console.error(e)
+      process.exit(-1)
     }
-
-    // // wait for catalog view to load
-    // try {
-    //     await page.waitForSelector('tbody > tr:nth-child(1) > td:nth-child(3) > a')
-    // } catch (e) {
-    //     console.log('workflow status does not exist')
-    // }
-    // await page.waitForTimeout(1000)
-    // // await page.screenshot({path: 'screenshot01_collection_view.png'})
-    // console.log("validating collection view: "+await test_collection_screenshot(page, 'screenshot01_collection_view', {
-    //     fullPage: true,
-    // }))
-    //
-    //
-    // // get workflow status and wait if its not complete
-    // const workflowStatusSelector = 'tbody > tr:nth-child(1) > td:nth-child(3) > a'
-    // await page.waitForSelector(workflowStatusSelector,  {polling:1000, timeout: 3000})
-    // try {
-    //     // Wait up to 5 seconds for the asset list to load
-    //     await page.waitForFunction(selector => !!document.querySelector(selector), {polling:1000, timeout: 3000}, workflowStatusSelector);
-    //     let workflowStatus = await page.evaluate(selector => document.querySelector(selector).innerText, workflowStatusSelector);
-    //     // wait up to 45 minutes for workflow to complete
-    //     let i = 45
-    //     console.log("Waiting for workflow to complete...")
-    //     while (workflowStatus != "Complete" && i > 0) {
-    //         // print the workflow status
-    //         workflowStatus = await page.evaluate(selector => document.querySelector(selector).innerText, workflowStatusSelector);
-    //         i = i - 1
-    //         await sleep(60000);
-    //         await page.goto(url,  {waitUntil: 'networkidle0'});
-    //         console.log(i + " minutes remaining")
-    //     }
-    //     console.log("workflowStatus: " + workflowStatus)
-    // } catch (e) {
-    //     console.log('Workflow status does not exist. Did you start a workflow?' + e)
-    // }
 
     // VALIDATE OBJECTS TAB
     console.log("Loading objects tab.")
@@ -190,105 +151,128 @@ let temp_password = process.env.TEMP_PASSWORD;
     // fullPage: true,
     // }))
 
-    //
-    // // VALIDATE CELEBRITY TAB
-    // let tab_selector='#__BVID__28___BV_tab_button__'
-    // await page.click(tab_selector)
-    // try {
-    //     await page.waitForSelector(rounded_button_selector, {timeout: 5000})
-    // } catch(e) {
-    //     // no data in this tab
-    // }
-    // // await page.screenshot({path: 'screenshot03_tab_celebrities.png'})
+
+    // VALIDATE CELEBRITY TAB
+    let tab_selector='#__BVID__28___BV_tab_button__'
+    await page.click(tab_selector)
+    try {
+        await page.waitForSelector(rounded_button_selector, {timeout: 5000})
+    } catch(e) {
+        // no data in this tab
+    }
     // console.log("validating celebrities: "+await test_screenshot_containing_video(page, 'screenshot03_tab_celebrities', {
     //     fullPage: true,
     // }))
-    //
-    // // VALIDATE MODERATION TAB
-    // tab_selector='#__BVID__30___BV_tab_button__'
-    // await page.click(tab_selector)
-    // try {
-    //     await page.waitForSelector(rounded_button_selector, {timeout: 5000})
-    // } catch(e) {
-    //     // no data in this tab
-    // }
-    // // await page.screenshot({path: 'screenshot04_tab_moderation.png'})
+
+    // VALIDATE MODERATION TAB
+    tab_selector='#__BVID__30___BV_tab_button__'
+    await page.click(tab_selector)
+    try {
+        await page.waitForSelector(rounded_button_selector, {timeout: 5000})
+    } catch(e) {
+        // no data in this tab
+    }
     // console.log("validating moderation: "+await test_screenshot_containing_video(page, 'screenshot04_tab_moderation', {
     //     fullPage: true,
     // }))
-    //
-    // // VALIDATE FACES TAB
-    // tab_selector='#__BVID__32___BV_tab_button__'
-    // await page.click(tab_selector)
-    // try {
-    //     await page.waitForSelector(rounded_button_selector, {timeout: 5000})
-    // } catch(e) {
-    //     // no data in this tab
-    // }
-    // // await page.screenshot({path: 'screenshot05_tab_faces.png'})
+
+    // VALIDATE FACES TAB
+    tab_selector='#__BVID__32___BV_tab_button__'
+    await page.click(tab_selector)
+    try {
+        await page.waitForSelector(rounded_button_selector, {timeout: 5000})
+    } catch(e) {
+        // no data in this tab
+    }
     // console.log("validating faces: "+await test_screenshot_containing_video(page, 'screenshot05_tab_faces', {
     //     fullPage: true,
     // }))
-    //
-    // // VALIDATE WORDS TAB
-    // tab_selector='#__BVID__34___BV_tab_button__'
-    // await page.click(tab_selector)
-    // try {
-    //     await page.waitForSelector(rounded_button_selector, {timeout: 3000})
-    // } catch(e) {
-    //     // no data in this tab
-    // }
-    // // await page.screenshot({path: 'screenshot06_tab_words.png'})
+
+    // VALIDATE WORDS TAB
+    tab_selector='#__BVID__34___BV_tab_button__'
+    await page.click(tab_selector)
+    try {
+        await page.waitForSelector(rounded_button_selector, {timeout: 3000})
+    } catch(e) {
+        // no data in this tab
+    }
     // console.log("validating words: "+await test_words_screenshot(page, 'screenshot06_tab_words', {
     //     fullPage: true,
     // }))
-    //
-    // // VALIDATE CUES TAB
-    // tab_selector='#__BVID__36___BV_tab_button__'
-    // await page.click(tab_selector)
-    // await page.waitForTimeout(3000)
-    // // await page.screenshot({path: 'screenshot07_tab_cues.png'})
+
+    // VALIDATE CUES TAB
+    tab_selector='#__BVID__36___BV_tab_button__'
+    await page.click(tab_selector)
+    await page.waitForTimeout(3000)
     // console.log("validating cues: "+await test_screenshot_containing_video(page, 'screenshot07_tab_cues', {
     //     fullPage: true,
     // }))
-    //
-    // // VALIDATE SHOTS TAB
-    // tab_selector='#__BVID__38___BV_tab_button__'
-    // await page.click(tab_selector)
-    // await page.waitForTimeout(3000)
-    // // await page.screenshot({path: 'screenshot08_tab_shots.png'})
+
+    // VALIDATE SHOTS TAB
+    tab_selector='#__BVID__38___BV_tab_button__'
+    await page.click(tab_selector)
+    await page.waitForTimeout(3000)
     // console.log("validating shots: "+await test_screenshot_containing_video(page, 'screenshot08_tab_shots', {
     //     fullPage: true,
     // }))
-    //
-    // // VALIDATE UPLOAD PAGE
-    // await page.goto(url + "/upload", {waitUntil: 'load'});
-    // await page.waitForSelector('#app > div > div.container > div.container > div > div > button.btn.m-1.btn-secondary.collapsed', {
-    //     visible: true,
-    // });
-    // page.once('load', () => console.log('Upload page loaded'));
-    //
-    // // Does the configure workflow form look right?
-    // await page.click('#app > div > div.container > div.container > div > div > button.btn.m-1.btn-secondary.collapsed');
-    // await page.waitForTimeout(500)
-    // // await page.screenshot({path: 'screenshot09_configure_workflow_form_default.png'})
-    // console.log("validating upload page: "+await test_screenshot_containing_video(page, 'screenshot09_configure_workflow_form_default', {
-    //     fullPage: true,
+
+    // VALIDATE TRANSCRIPT TAB
+    tab_selector='#__BVID__40___BV_tab_button__'
+    await page.click(tab_selector)
+    await page.waitForTimeout(3000)
+    // console.log("validating transcript: "+await test_screenshot_containing_video(page, 'screenshot09_transcript', {
+    //   fullPage: true,
     // }))
-    //
-    // await page.click('#collapse-2 > div > div:nth-child(2) > button:nth-child(2)');
-    // await page.waitForTimeout(500)
-    // // await page.screenshot({path: 'screenshot10_configure_workflow_form_clear_all.png'})
-    // console.log("validating workflow config form: "+await test_screenshot_containing_video(page, 'screenshot10_configure_workflow_form_clear_all', {
-    //     fullPage: true,
+
+    // VALIDATE TRANSLATION TAB
+    tab_selector='#__BVID__45___BV_tab_button__'
+    await page.click(tab_selector)
+    await page.waitForTimeout(3000)
+    // console.log("validating translation: "+await test_screenshot_containing_video(page, 'screenshot10_translation', {
+    //   fullPage: true,
     // }))
-    //
-    // await page.click('#collapse-2 > div > div:nth-child(2) > button:nth-child(1)');
-    // await page.waitForTimeout(500)
-    // // await page.screenshot({path: 'screenshot11_configure_workflow_form_select_all.png'})
-    // console.log("validating clear all: "+await test_screenshot(page, 'screenshot11_configure_workflow_form_select_all', {
-    //     fullPage: true,
+
+    // VALIDATE TRANSLATE TAB
+    tab_selector='#__BVID__47___BV_tab_button__'
+    await page.click(tab_selector)
+    await page.waitForTimeout(3000)
+    // console.log("validating key phrases: "+await test_screenshot_containing_video(page, 'screenshot11_keyphrases', {
+    //   fullPage: true,
     // }))
+
+    // VALIDATE TRANSLATE TAB
+    tab_selector='#__BVID__49___BV_tab_button__'
+    await page.click(tab_selector)
+    await page.waitForTimeout(3000)
+    // console.log("validating entities: "+await test_screenshot_containing_video(page, 'screenshot12_entities', {
+    //   fullPage: true,
+    // }))
+
+  // VALIDATE UPLOAD PAGE
+    await page.goto(url + "/upload", {waitUntil: 'load'});
+    await page.waitForSelector('#app > div > div.container > div.container > div > div > button.btn.m-1.btn-secondary.collapsed', {
+        visible: true,
+    });
+    page.once('load', () => console.log('Upload page loaded'));
+
+    // Does the configure workflow form look right?
+    await page.click('#app > div > div.container > div.container > div > div > button.btn.m-1.btn-secondary.collapsed');
+    await page.waitForTimeout(500)
+    console.log("validating upload page: "+await test_screenshot_containing_video(page, 'screenshot13_configure_workflow_form_default', {
+        fullPage: true,
+    }))
+
+    await page.click('#collapse-2 > div > div:nth-child(2) > button:nth-child(2)');
+    await page.waitForTimeout(500)
+    console.log("validating workflow config form: "+await test_screenshot_containing_video(page, 'screenshot14_configure_workflow_form_clear_all', {
+        fullPage: true,
+    }))
+
+    await page.click('#collapse-2 > div > div:nth-child(2) > button:nth-child(1)');
+    await page.waitForTimeout(500)
+    console.log("validating clear all: "+await test_screenshot(page, 'screenshot15_configure_workflow_form_select_all', {
+        fullPage: true,
+    }))
 
     await browser.close();
     console.log("Done")
